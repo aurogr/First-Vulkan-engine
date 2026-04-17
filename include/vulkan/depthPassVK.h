cd1b9a51e16e151d2842b5716870870daf412315
bdevainy <auro.g.r.14@gmail.com>
2026-04-17 13:10:34 +0200

    working part 1

--- include/vulkan/depthPassVK.h
+++ include/vulkan/depthPassVK.h
@@ -1,37 +1,66 @@
 #pragma once
 
-#include "common.h"
+#include "vulkan/renderPassVK.h"
 
 namespace MiniEngine
 {
     struct Runtime;
     class Entity;
-    struct Frame;
     typedef std::shared_ptr<Entity> EntityPtr;
 
-    class RenderPassVK
+    class DepthPassVK final : public RenderPassVK
     {
     public:
-        RenderPassVK( const Runtime& i_runtime, const std::shared_ptr<RenderPassVK> i_prev_pass = nullptr ) : 
-            m_runtime( i_runtime ),
-            m_prev_render_pass( i_prev_pass )
-        {}
-        virtual ~RenderPassVK() = default;
+        DepthPassVK(
+            const Runtime& i_runtime,
+            const ImageBlock& i_depth_buffer);
+        virtual ~DepthPassVK();
 
-        virtual bool            initialize() = 0;
-        virtual void            shutdown  () = 0;
-        virtual VkCommandBuffer draw      ( const Frame& ) = 0;
+        bool            initialize() override;
+        void            shutdown() override;
+        VkCommandBuffer draw(const Frame& i_frame) override;
 
-        virtual void addEntityToDraw( const EntityPtr i_entity )
-        {
-        }
-
-    protected:
-        const Runtime& m_runtime;
-        const std::shared_ptr<RenderPassVK> m_prev_render_pass;
+        void addEntityToDraw(const EntityPtr i_entity) override;
 
     private:
-        RenderPassVK( const RenderPassVK& ) = delete;
-        RenderPassVK& operator=(const RenderPassVK& ) = delete;
+        DepthPassVK(const DepthPassVK&) = delete;
+        DepthPassVK& operator=(const DepthPassVK&) = delete;
+
+        void createFbo();
+        void createRenderPass();
+        void createPipelines();
+        void createDescriptorLayout();
+        void createDescriptors();
+
+        struct DescriptorsSets
+        {
+            VkDescriptorSet m_per_frame_descriptor;
+            VkDescriptorSet m_per_object_descriptor;
+        };
+
+        struct MaterialPipeline
+        {
+            // prepare the different render supported depending on the material
+            VkPipeline                                                         m_pipeline;
+            VkPipelineLayout                                                   m_pipeline_layouts;
+            std::array<VkDescriptorSetLayout, 2                    > m_descriptor_set_layout; //2 sets, per frame and per object
+            std::array<DescriptorsSets, 3                    > m_descriptor_sets;
+            std::array<VkPipelineShaderStageCreateInfo, 2                    > m_shader_stages;
+        };
+
+        std::array<MaterialPipeline, 2> m_pipelines; //one by material
+
+        VkRenderPass                   m_render_pass;
+        std::array<VkCommandBuffer, 3> m_command_buffer;
+        std::array<VkFramebuffer, 3> m_fbos;
+        VkDescriptorPool               m_descriptor_pool;
+
+        std::unordered_map<uint32_t, std::vector<EntityPtr>> m_entities_to_draw;
+
+        const ImageBlock m_depth_buffer;
+        const ImageBlock m_color_attachment;
+        const ImageBlock m_normals_attachment;
+        const ImageBlock m_position_attachment;
+        const ImageBlock m_material_attachment;
     };
 };
