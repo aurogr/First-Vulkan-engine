ee8d330cebed6ec88e51055e9ca1560b2f1cfe6f
bdevainy <auro.g.r.14@gmail.com>
2026-04-17 18:53:03 +0200

    tried to change

--- include/vulkan/depthPassVK.h
+++ include/vulkan/depthPassVK.h
@@ -8,13 +8,13 @@ namespace MiniEngine
     class Entity;
     typedef std::shared_ptr<Entity> EntityPtr;
 
-    class DepthPassVK final : public RenderPassVK
+    class DepthPrePassVK final : public RenderPassVK
     {
     public:
-        DepthPassVK(
+        DepthPrePassVK(
             const Runtime& i_runtime,
             const ImageBlock& i_depth_buffer);
-        virtual ~DepthPassVK();
+        virtual ~DepthPrePassVK();
 
         bool            initialize() override;
         void            shutdown() override;
@@ -23,8 +23,8 @@ namespace MiniEngine
         void addEntityToDraw(const EntityPtr i_entity) override;
 
     private:
-        DepthPassVK(const DepthPassVK&) = delete;
-        DepthPassVK& operator=(const DepthPassVK&) = delete;
+        DepthPrePassVK(const DepthPrePassVK&) = delete;
+        DepthPrePassVK& operator=(const DepthPrePassVK&) = delete;
 
         void createFbo();
         void createRenderPass();
@@ -38,23 +38,24 @@ namespace MiniEngine
             VkDescriptorSet m_per_object_descriptor;
         };
 
-        struct DepthPipeline
+        struct MaterialPipeline
         {
-            VkPipeline                              m_pipeline;
-            VkPipelineLayout                        m_pipeline_layouts;
-            std::array<VkDescriptorSetLayout, 2 >   m_descriptor_set_layout; //2 sets, per frame and per object
-            std::array<DescriptorsSets, 3 >         m_descriptor_sets; // one per flame in flight
-            VkPipelineShaderStageCreateInfo         m_shader_stages; // only vertex
+            // prepare the different render supported depending on the material
+            VkPipeline                                                         m_pipeline;
+            VkPipelineLayout                                                   m_pipeline_layouts;
+            std::array<VkDescriptorSetLayout, 2                    > m_descriptor_set_layout; //2 sets, per frame and per object
+            std::array<DescriptorsSets, 3                    > m_descriptor_sets;
+            VkPipelineShaderStageCreateInfo m_shader_stage;
         };
 
-        DepthPipeline depth_pipeline; // just one pipeline, we don't care about materials on the depth pass
+        std::array<MaterialPipeline, 2> m_pipelines; //one by material
 
         VkRenderPass                   m_render_pass;
         std::array<VkCommandBuffer, 3> m_command_buffer;
-        std::array<VkFramebuffer, 3> m_depth_fbos;
+        std::array<VkFramebuffer, 3> m_fbos;
         VkDescriptorPool               m_descriptor_pool;
 
-        std::vector<EntityPtr> m_entities_to_draw;
+        std::unordered_map<uint32_t, std::vector<EntityPtr>> m_entities_to_draw;
 
         const ImageBlock m_depth_buffer;
     };
